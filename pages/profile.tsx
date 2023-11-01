254905feff3d7197a5d182dd7dcb556fb5fcfd29
mohansharma916 <mohansharma916@gmail.com>
2023-11-01 11:59:17 +0530

    profile updated

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -10,14 +10,42 @@ import IsAuth from "@/src/components/IsAuth";
 import { useState } from "react";
 
 const Profile = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedProfilePhoto, setSelectedProfilePhoto] = useState(null);
+  const [selectedAadharFrontPhoto, setSelectedAadharFrontPhoto] =
+    useState(null);
+  const [selectedAadharBackPhoto, setSelectedAadharBackPhoto] = useState(null);
+  const [selectedPaymentSlip, setSelectedPaymentSlip] = useState(null);
 
-  const handleFileChange = (e) => {
+  const handleProfilePhotoChange = (e) => {
     const file = e.target.files[0];
 
     if (file) {
       const imageUrl = URL.createObjectURL(file);
-      setSelectedImage(imageUrl);
+      setSelectedProfilePhoto(imageUrl);
+    }
+  };
+  const handleAadharFrontPhotoChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedAadharFrontPhoto(imageUrl);
+    }
+  };
+  const handleAadharbackPhotoChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedAadharBackPhoto(imageUrl);
+    }
+  };
+  const handlePaymentSlipChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedPaymentSlip(imageUrl);
     }
   };
 
@@ -84,10 +112,29 @@ const Profile = () => {
             <div className="w-full flex m-4">
               <div className=" w-60 h-full  ">
                 <div className=" h-4/5  w-4/5 items-center flex justify-center bg-gray-500">
-                  <UserCircleIcon className="h-28 w-28" aria-hidden="true" />
+                  {selectedProfilePhoto ? (
+                    <div className="image-preview">
+                      <img
+                        src={selectedProfilePhoto}
+                        className="h-32 w-32"
+                        alt="Uploaded"
+                      />
+                    </div>
+                  ) : (
+                    <UserCircleIcon className="h-28 w-28" aria-hidden="true" />
+                  )}
                 </div>
                 <div className="flex w-4/5 bg-[#FAA935] text-white justify-center mt-5 border-2 rounded-md">
-                  <p className="h-22 ">Update Profile Photo</p>
+                  <label className="h-22" htmlFor="profile_uploads">
+                    Update Profile Photo
+                  </label>
+                  <input
+                    id="profile_uploads"
+                    type="file"
+                    name="profile_uploads"
+                    className="hidden"
+                    onChange={handleProfilePhotoChange}
+                  />
                 </div>
               </div>
               <div className="px-12 py-5">
@@ -102,37 +149,83 @@ const Profile = () => {
             <div className="bg-white w-3/5 h-60 rounded-md px-6 py-2">
               <h2 className="text-xl underline-offset-4">Documents</h2>
               <div className="flex flex-col">
-                <div className="gap-5 ">
-                  <p>Aadhar Front Side</p>
-                  <input
-                    type="file"
-                    name="myImage"
-                    onChange={handleFileChange}
-                  />
-                  {selectedImage && (
+                <div className="gap-5 flex my-2 ">
+                  <div className="flex">
+                    <p>Aadhar Front Side</p>
+                    <div className="bg-[#FAA935] text-center w-40 text-white justify-center border-2 rounded-md">
+                      <label className="h-22" htmlFor="aadhar_front">
+                        Update Front Side
+                      </label>
+                      <input
+                        id="aadhar_front"
+                        type="file"
+                        name="aadhar_front"
+                        className="hidden"
+                        onChange={handleAadharFrontPhotoChange}
+                      />
+                    </div>
+                    {selectedAadharFrontPhoto && (
+                      <div className="image-preview">
+                        <img
+                          src={selectedAadharFrontPhoto}
+                          className="h-32 w-32"
+                          alt="Uploaded"
+                        />
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="gap-5 flex my-2">
+                  <p>Aadhar Back Side</p>
+                  <div className="bg-[#FAA935]  text-center w-52 text-white justify-center border-2 rounded-md">
+                    <label className="h-22" htmlFor="aadhar_back">
+                      Upload Aadhar Back Side
+                    </label>
+                    <input
+                      id="aadhar_back"
+                      type="file"
+                      name="aadhar_back"
+                      className="hidden"
+                      onChange={handleAadharbackPhotoChange}
+                    />
+                  </div>
+                  {selectedAadharBackPhoto && (
                     <div className="image-preview">
-                      <img src={selectedImage} alt="Uploaded" />
+                      <img
+                        src={selectedAadharBackPhoto}
+                        className="h-32 w-32"
+                        alt="Uploaded"
+                      />
                     </div>
                   )}
-                  {/* <p className="p-1 bg-[#FAA935] text-center">Upload</p> */}
-                </div>
-                <div className="gap-5">
-                  <p>Aadhar Back Side</p>
-                  <input
-                    type="file"
-                    name="myImage"
-                    onChange={handleFileChange}
-                  />
 
                   {/* <p className="p-1 bg-[#FAA935] text-center">Upload</p> */}
                 </div>
-                <div className="gap-5">
+                <div className="gap-5 flex my-2">
                   <p>Payment Slip</p>
-                  <input
-                    type="file"
-                    name="myImage"
-                    onChange={handleFileChange}
-                  />
+                  <div className="bg-[#FAA935] text-center w-52 text-white justify-center border-2 rounded-md">
+                    <label className="h-22" htmlFor="payment_uploads">
+                      Update Payment Slip
+                    </label>
+                    <input
+                      id="payment_uploads"
+                      type="file"
+                      name="payment_uploads"
+                      className="hidden"
+                      onChange={handlePaymentSlipChange}
+                    />
+                  </div>
+                  {selectedPaymentSlip && (
+                    <div className="image-preview">
+                      <img
+                        src={selectedPaymentSlip}
+                        className="h-32 w-32"
+                        alt="Uploaded"
+                      />
+                    </div>
+                  )}
+
                   {/* <p className="p-1 bg-[#FAA935] text-center">Upload</p> */}
                 </div>
               </div>
@@ -141,7 +234,10 @@ const Profile = () => {
               Upcoming Trips
             </div>
           </div>
-          <div className="bg-red-800 mt-10">Your Trips</div>
+          <div className="bg-white px-6 py-2 mt-10 h-64 w-full">
+            <p>Your Trips</p>
+            <p className="text-3xl"> There is No Trip</p>
+          </div>
         </div>
       </div>
     </>
